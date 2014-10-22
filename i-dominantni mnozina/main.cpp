9fd7a2963cf3829f56df86c0c5632c6554a783c6
Jan Zaloudek <mail@janzaloudek.cz>
2014-10-22 10:58:30 +0200

    Initial Commit

---
+++ i-dominantni mnozina/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  i-dominantni mnozina
+//
+//  Created by Jan Zaloudek on 22/10/14.
+//  Copyright (c) 2014 Jan Zaloudek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
